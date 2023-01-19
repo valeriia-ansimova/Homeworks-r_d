c198eeb020b9c390ec3aeefdd04e9a108cf271ec
Valery <valery.ansimova@gmail.com>
2023-01-19 17:56:04 +0100

    Homework lesson 3

---
+++ src/java/homework3/Arithmetic.java
@@ -0,0 +1,13 @@
+package homework3;
+
+public class Arithmetic {
+
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 5;
+        System.out.println("Sum: " + (a + b));
+        System.out.println("Minus: " + (a - b));
+        System.out.println("Multiply: " + (a * b));
+        System.out.println("Dividing: " + (a / b));
+    }
+}
