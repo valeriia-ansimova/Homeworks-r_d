c154088f75406b047d7e2d7448f49d5e56b3b270
Valery <valery.ansimova@gmail.com>
2023-05-03 20:11:23 +0200

    Homework 27 Lesson 33

---
+++ src/java/homework27/Main.java
@@ -0,0 +1,9 @@
+package homework27;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new DigitsGenerator());
+        thread.start();
+    }
+}
